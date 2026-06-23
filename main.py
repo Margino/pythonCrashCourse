422413fb0fc7f888668f0d866537c51ce07cb86b
Alex <marginoru@gmail.com>
2026-06-24 00:12:00 +0300

    урок 2: Базовые операции

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Hello World!")
+# This is a comment
+print("number:",5, "'some text'", sep=" | ", end=" ... ")
+print("third line", end="\n")
+print("fourth line")
+# print("Reuslt:", input("Input something:"))
+print("Number 136 contains", 137//7 ,"numbers seven")
