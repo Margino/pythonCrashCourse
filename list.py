d7d832ccded00ea96c97534dea4e8b647023ff91
Alex <marginoru@gmail.com>
2026-06-25 15:26:19 +0300

    удаление элементов списка

---
+++ list.py
@@ -0,0 +1,14 @@
+names = ['Alex', 'John', 'Ray']
+print(names[0])
+print(names[1])
+print(names[-1])
+
+print(f"My friend's name is {names[0]}")
+print(f"My friend's name is {names[1]}")
+print(f"My friend's name is {names[2]}")
+
+my_favorite_transports = ["Honda", "BMW", "Opel"]
+message = "I'd like to buy"
+print(f"{message} '{my_favorite_transports[0]}'")
+print(f"{message} '{my_favorite_transports[1]}'")
+print(f"{message} '{my_favorite_transports[2]}'")
---
+++ list_el.py
@@ -0,0 +1,68 @@
+def sent_invites(guests):
+    if not guests:
+        print("Список гостей пустой!")
+        return
+
+    for guest in guests:
+        print(f"Дорогой {guest}, приглашем тебя на вечеринку!")
+
+
+def change_guest(guests, old_guest, new_guest):
+
+    if old_guest not in guests:
+        print()
+        print("Замена не произведена. Нет гостя которого надо удалить!")
+        return guests
+    
+    for i in range(len(guests)):
+        if guests[i] == old_guest:
+            guests[i] = new_guest
+            break
+
+    print()
+    print(f"Так как {old_guest} не смог придти, к нам придёт {new_guest}")
+
+    return guests
+
+
+guests_list = ['Иван', 'Олег', 'Глеб', 'Яков', 'Мирон', 'Миша', 'Глеб']
+
+print()
+print("Cписок приглашений:")
+sent_invites(guests_list)
+
+change_guest(guests_list, 'Глеб', '*Матвей*')
+
+print()
+print("Новый список приглашений:")
+sent_invites(guests_list)
+
+print()
+print("Ура мы нашли стол большего размера! Теперь мы ждём больше гостей!!!")
+
+guests_list.insert(0,'-Слава-')
+guests_list.insert(len(guests_list)//2,'-Ярик-')
+guests_list.append('-Жора-')
+
+print()
+print("Новый список приглашений:")
+sent_invites(guests_list)
+
+
+guests_remained = 2
+print()
+print(f"К сожалению привести новый стол не успевают. Не вечеринку приглашены только {guests_remained} гостя :(\n")
+
+for i in range(len(guests_list) - guests_remained):
+    print(f"Дорогой {guests_list.pop()}, к сожалнию, ваше приглашение на вечеринку отменено.")
+
+
+print()
+for guest in guests_list:
+    print(f"Дорогой {guest}, ваше приглашение на вечеринку остаётся в силе!")
+
+while guests_list:
+    del guests_list[0]
+
+print()
+print("Гости закончились!", guests_list)
